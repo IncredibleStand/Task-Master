ff9a919a81ef8b93386d4910153758076450a64f
IncredibleStand <incrediblestand@gmail.com>
2025-03-24 10:03:44 +0100

    Update layout.tsx
    Added <Analytics /> and <SpeedInsights /> to your layout’s JSX to enable Vercel’s tracking features.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen bg-gray-100`}>{children}</body>
+      <body className={`${inter.className} min-h-screen bg-gray-100`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
